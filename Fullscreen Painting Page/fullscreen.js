48bd2a23cdcc046b9020ff698219bffcb03633bb
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2025-05-06 19:15:53 -0700

    Prep for actually changing the painting

--- Fullscreen Painting Page/fullscreen.js
+++ Fullscreen Painting Page/fullscreen.js
@@ -26,10 +26,31 @@ function hideDescription(){
   dB.removeEventListener('click',hideDescription);
   dB.addEventListener('click',viewDescription);
 }
+function setPainting(){ //read the url to find the specific painting
+  const URLparams = window.location.search("painting");
+  //search for the background version and the backgroundless version
+  //Defaults
+  var paintingTitle = "Painting Title";
+  var family = "Part 1 : The Human Family Tree (Christians)";
+  var paintingSrc = "./public/1a-4@2x.png";
+  var paintingDescription = "";
+  if (URLparams){
+    //here we would check the corresponding image and update the title accordingly
+  }
+  document.getElementById('paintingTitle').innerText = paintingTitle;
+  document.getElementById('artworkDescription').innerText = family;
+  document.getElementById('painting').src = paintingSrc;
+  //probably change size too
+  document.getElementById("descriptionText").innerText = paintingDescription;
+}
 function previousPainting(){ //may need to change paintings size
+  //Things that will need to change:
+  // Painting Title, Description Composition, Description Dimensions, Actual Descripton, Painting Family
 
 }
 function nextPainting(){ //may need to change paintings size
+  //Things that will need to change:
+  // Painting Title, Description Composition, Description Dimensions, Actual Descripton, Painting Family
 
 }
 function exitFullScreen(){
